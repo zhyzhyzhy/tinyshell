8047c8b84b61b9820d4e750628fdb61522d0788e
zhyzhyzhy <1012146386@qq.com>
2017-05-12 12:03:18 +0800

    add IOredirect

--- littleShell/process.c
+++ littleShell/process.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include "process.h"
 #include "env.h"
 
@@ -51,15 +52,36 @@ launch_process(process *p, pid_t pgid,
     char read[1024];
     env_path *path1 = path;
 
+    int outfd = -1;
+    int infd = -1;
 
-    /*
-     *
-     * todo
-     * redirect
-     *
-     *
-     * */
-
+    for (int i = 0; p->argv[i] != NULL ; ++i) {
+        if ((p->argv[i])[0] == '>') {
+            if (p->argv[i][1] == '>') {
+                outfd = open(p->argv[i+1], O_RDWR | O_APPEND);
+            }
+            else {
+                outfd = open(p->argv[i+1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+            }
+            if (outfd == -1) {
+                perror(p->argv[i+1]);
+            }
+            p->argv[i] = NULL;
+        }
+        else if ((p->argv[i])[0] == '<') {
+            infd = open(p->argv[i+1], O_RDONLY);
+            if (infd == -1) {
+                perror(p->argv[i+1]);
+            }
+            p->argv[i] = NULL;
+        }
+    }
+    if (outfd != -1) {
+        dup2(outfd, STDOUT_FILENO);
+    }
+    if (infd != -1) {
+        dup2(infd, STDIN_FILENO);
+    }
     while (p != NULL) {
         strcpy(read, path->path);
         strcat(read, p->argv[0]);
@@ -73,6 +95,13 @@ launch_process(process *p, pid_t pgid,
     }
     if (execv(read, p->argv) == -1) {
         perror("exec");
+    }
+
+    if (outfd != -1) {
+        close(outfd);
+    }
+    if (infd != -1) {
+        close(infd);
     }
 }
 
